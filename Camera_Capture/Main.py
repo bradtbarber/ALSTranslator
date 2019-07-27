f3cfccf748f750eeddaf97f66761da4136547aa9
bradleyb <bradleyb@sfu.ca>
2019-07-27 13:10:52 -0700

    Modified Camera_Capture script to write each picture as a separate line in the output csv

--- Camera_Capture/Main.py
+++ Camera_Capture/Main.py
@@ -5,21 +5,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 
-def write_image_as_csv(image, image_name):
-	#get size of the image
-	size = image.size
-
-	#open a file to write the pixel data
+def write_csv_columns(image_name):
+	#open a file to write the column names
 	with open('.\\output\\csv\\' + image_name + '.csv', 'w+') as f:
-		#read each pixel value and write to the file
-		x = 0
-		y = 0
-		for i in range(size):
+		#write column names
+		for i in range(784):
 			f.write('pixel' + str(i + 1))
-			if size != i + 1:
+			if 784 != i + 1:
 				f.write(',')
 		f.write('\n')
 
+def write_image_as_csv(image, image_name):
+	#open a file to write the pixel data
+	with open('.\\output\\csv\\' + image_name + '.csv', 'a') as f:
+		#read each pixel value and write to the file
+		x = 0
+		y = 0
 		for y in range(28):
 			for x in range(28):
 				p = image[y][x]
@@ -29,36 +30,52 @@ def write_image_as_csv(image, image_name):
 		f.write('\n')
 
 def capture_image_and_save_as_csv():
-	while 1:
-		if msvcrt.kbhit():
-			if ord(msvcrt.getch()) == 27:
-				print(ord(msvcrt.getche()))
-				break
-			if ord(msvcrt.getch()) == 32:
-				print('Testing..')
-				print(ord(msvcrt.getche()))
-		# body of the loop ...
-			date = datetime.datetime.now().strftime('_%Y%m%d_%H-%M-%S')
-			image_name = 'test' + date
-			
-			cap = cv2.VideoCapture(0)
-			print('Capturing Image')
-			if cap.isOpened():
-				ret, frame = cap.read()
-			else:
-				ret = False
-				print('ERROR: Video capture device could not be found.')
-				return -1
-			
-			gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY)
-			grayscale = cv2.resize(gray, dsize = (28, 28), interpolation = cv2.INTER_CUBIC)
+	#generate file name for csv output
+	date = datetime.datetime.now().strftime('_%Y%m%d_%H-%M-%S')
+	image_name = 'test' + date
 
-			write_image_as_csv(grayscale, image_name)
-					
-			cv2.imwrite('.\\output\\png\\' + image_name + '.png', grayscale)
+	#retreive camera
+	cap = cv2.VideoCapture(0)
+
+	#prepare output csv columns
+	write_csv_columns(image_name)
+
+	picture_num = 0
+	while 1:
+    	#wait for keyboard interrupt
+		if msvcrt.kbhit():
+    		#if 'esc' key detected, exit loop
+			if ord(msvcrt.getch()) == 27:
+				print('Exiting...')
+				break
+
+			#if `spacebar` key detected, take picture
+			if ord(msvcrt.getch()) == 32:
+				print('Capturing Image...')
+				if cap.isOpened():
+					ret, frame = cap.read()
+				else:
+					ret = False
+					print('ERROR: Video capture device could not be found.')
+					return -1
 			
-			cap.release()
-	return '.\\output\\'
+				#convert image to greyscale and scale to 784 pix
+				grayscale = cv2.resize(
+					cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY), 
+					dsize = (28, 28), 
+					interpolation = cv2.INTER_CUBIC
+					)
+
+				#write picture pix values as new row to csv
+				write_image_as_csv(grayscale, image_name)
+
+				#write image to output directory for debugging and verification		
+				cv2.imwrite('.\\output\\png\\' + image_name + '_' + str(picture_num) + '.png', grayscale)
+				picture_num = picture_num + 1
+	
+	#release camera and return path to csv
+	cap.release()
+	return '.\\output\\csv\\' + image_name + '.csv'
 
 def main():
 	image_path = capture_image_and_save_as_csv()
