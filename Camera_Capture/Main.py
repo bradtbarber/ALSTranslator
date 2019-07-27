3f647e335884c8dfbbeda30c04c3399dc00edf60
PK-GH <pkvac@sfu.ca>
2019-07-26 17:20:15 -0700

    updated image capture to take multiple pictures

--- Camera_Capture/Main.py
+++ Camera_Capture/Main.py
@@ -1,4 +1,5 @@
 import csv
+import msvcrt
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,33 +29,43 @@ def write_image_as_csv(image, image_name):
 		f.write('\n')
 
 def capture_image_and_save_as_csv():
-	date = datetime.datetime.now().strftime('_%Y%m%d_%H-%M-%S')
-	image_name = 'test' + date
-	
-	cap = cv2.VideoCapture(0)
-	print('Capturing Image')
-	if cap.isOpened():
-		ret, frame = cap.read()
-	else:
-		ret = False
-		print('ERROR: Videa capture device could not be found.')
-		return -1
-	
-	gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY)
-	grayscale = cv2.resize(gray, dsize = (28, 28), interpolation = cv2.INTER_CUBIC)
-
-	write_image_as_csv(grayscale, image_name)
+	while 1:
+		if msvcrt.kbhit():
+			if ord(msvcrt.getch()) == 27:
+				print(ord(msvcrt.getche()))
+				break
+			if ord(msvcrt.getch()) == 32:
+				print('Testing..')
+				print(ord(msvcrt.getche()))
+		# body of the loop ...
+			date = datetime.datetime.now().strftime('_%Y%m%d_%H-%M-%S')
+			image_name = 'test' + date
 			
-	cv2.imwrite('.\\output\\png\\' + image_name + '.png', grayscale)
-	cap.release()
-	return '.\\output\\' + image_name + '.png'
+			cap = cv2.VideoCapture(0)
+			print('Capturing Image')
+			if cap.isOpened():
+				ret, frame = cap.read()
+			else:
+				ret = False
+				print('ERROR: Video capture device could not be found.')
+				return -1
+			
+			gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY)
+			grayscale = cv2.resize(gray, dsize = (28, 28), interpolation = cv2.INTER_CUBIC)
+
+			write_image_as_csv(grayscale, image_name)
+					
+			cv2.imwrite('.\\output\\png\\' + image_name + '.png', grayscale)
+			
+			cap.release()
+	return '.\\output\\'
 
 def main():
 	image_path = capture_image_and_save_as_csv()
 	if (image_path == -1):
 		print("ERROR: Image could not be captured")
 	else:
-		print("Image saved to: " + image_path)
+		print("Images saved to: " + image_path)
 
 if __name__ == "__main__":
 	main()
